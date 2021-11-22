a92d1569e44740ad048544a924071a9a1080120e
ahmedElshiekh <ahmedel.sh50@gmail.com>
2021-11-22 03:32:49 +0200

    add scripting

---
+++ build/CMakeFiles/gamechess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gamechess.dir/main.cpp.o"
+  "gamechess"
+  "gamechess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gamechess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+
+using namespace sf;
+
+
+// Main Function
+int main() 
+{
+    RenderWindow window(VideoMode(45,45), "The Chass!");
+    
+    sf::Texture t1 ;
+    t1.loadFromFile("img/figures.png");
+    
+    Sprite s(t1);
+    
+    
+    //  Windows open event
+    while (window.isOpen())
+    {
+        
+        Event e;
+        while (window.pollEvent(e))
+        {
+            if (e.type == Event::Closed){
+                window.close();
+        }
+        
+        
+        // Drowing Window
+        window.clear();
+        window.draw(s);
+        window.display();  
+        }
+        
+    }//End while
+    
     return 0;
-}
+}// End Main
